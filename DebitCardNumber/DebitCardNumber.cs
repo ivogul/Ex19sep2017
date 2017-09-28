c2a588bcee6da829211f8bc8ca0de5f18b4329c2
ivogul <ivogul@gmail.com>
2017-09-28 21:19:55 +0300

    Added comments in DebitCardNumber

--- DebitCardNumber/DebitCardNumber.cs
+++ DebitCardNumber/DebitCardNumber.cs
@@ -10,10 +10,10 @@ namespace DebitCardNumber
     {
         static void Main(string[] args)
         {
-            int first = int.Parse(Console.ReadLine());
-            int second = int.Parse(Console.ReadLine());
-            int third = int.Parse(Console.ReadLine());
-            int forth = int.Parse(Console.ReadLine());
+            int first = int.Parse(Console.ReadLine()); //Reads first digits
+            int second = int.Parse(Console.ReadLine()); //Reads second digits
+            int third = int.Parse(Console.ReadLine()); //Reads third digits
+            int forth = int.Parse(Console.ReadLine()); //Reads forth digits
 
             Console.WriteLine($"{first:D4} {second:D4} {third:D4} {forth:D4}");
         }
